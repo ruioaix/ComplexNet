5eb8203cd675386b22f3abff37f59e95be63efb6
xrfind <xrfind@gmail.com>
2013-12-10 18:03:23 +0100

    great script

--- Main/recommend201312/find_best_lambda_of_hybrid/generateScripts.php
+++ Main/recommend201312/find_best_lambda_of_hybrid/generateScripts.php
@@ -7,21 +7,77 @@
   $loopNum = $argv[6];
   $directory = $argv[7];
 
+  $datafile = $filenamePre . "_data";
+  $outputdir = $filenamePre . "_results";
   $subfp = fopen($filenamePre . "_qsubscript", "w");
+  $dataprocessfp = fopen($filenamePre . "_dataprocess", "w");
+  fprintf($dataprocessfp, "#!/bin/bash\n");
   $namePost = 0;
   while($namePost < $end) {
-    $filename = $directory . '/' . $filenamePre . $namePost;
+    $filename = $directory. '/' . $filenamePre . $namePost;
     $fp = fopen($filename, "w") or die("couldn't open $filename");
-    fprintf($fp, "sourcecode/run $netfilename $loopNum $namePost $stepLen $stepNum");
+    fprintf($fp, "#!/bin/bash\n");
+    fprintf($fp, "./sourcecode/run $netfilename $loopNum $namePost $stepLen $stepNum | tee $outputdir/$filenamePre$namePost\n");
     fclose($fp);
-    fprintf($subfp, "qsub -cwd $filename\n");
+    fprintf($subfp, "qsub -cwd ./$filename \n");
+    if ($namePost == 0) {
+      fprintf($dataprocessfp, "cat $outputdir/$filenamePre$namePost |grep hybrid > $outputdir/$datafile\n");
+    }
+    else {
+      fprintf($dataprocessfp, "cat $outputdir/$filenamePre$namePost |grep hybrid >> $outputdir/$datafile\n");
+    }
     $namePost += $stepLen*$stepNum;
   }
   fclose($subfp);
 
-  //php generateScripts.php movielens 0.01 10 1 data/movielen/movielens.txt 100 scripts_movielens
-  //php generateScripts.php netflix 0.01 10 1 data/netflix/netflix_with_rating.txt 100 scripts_netflix
-  //php generateScripts.php rym 0.01 10 1 data/rym/rym_clean 100 scripts_rym
+  fprintf($dataprocessfp, "gnuplot << EOF
+
+set terminal postscript eps
+
+set xlabel 'theta'
+set ylabel 'top L=50 score average'
+set output '$outputdir/averagescore_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:17 w lp pt 19 ps 0.2 lc 3 title 'averagescore_hybrid'
+set output
+
+set xlabel 'theta'
+set ylabel 'RankScore'
+set output '$outputdir/RankScore_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:7 w lp pt 19 ps 0.2 lc 3 title 'RankScore_hybrid'
+set output
+
+set xlabel 'theta'
+set ylabel 'Precision'
+set output '$outputdir/Precision_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:9 w lp pt 19 ps 0.2 lc 3 title 'Precision_hybrid'
+set output
+
+set xlabel 'theta'
+set ylabel 'Intrasimilarity'
+set output '$outputdir/Intrasimilarity_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:11 w lp pt 19 ps 0.2 lc 3 title 'Intrasimilarity_hybrid'
+set output
+
+set xlabel 'theta'
+set ylabel 'HammingD'
+set output '$outputdir/HammingD_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:13 w lp pt 19 ps 0.2 lc 3 title 'HammingD_hybrid'
+set output
+
+set xlabel 'theta'
+set ylabel 'Popularity'
+set output '$outputdir/Popularity_hybrid_$filenamePre.eps'
+plot '$outputdir/$datafile' using 3:15 w lp pt 19 ps 0.2 lc 3 title 'Popularity_hybrid'
+set output
+
+EOF
+");
+
+  fclose($dataprocessfp);
+
+  //php generateScripts.php movielens 0.01 5 1.01 data/movielen/movielens.txt 100 scripts_movielens
+  //php generateScripts.php netflix 0.01 5 1.01 data/netflix/netflix_with_rating.txt 100 scripts_netflix
+  //php generateScripts.php rym 0.01 5 1.01 data/rym/rym_clean 100 scripts_rym
   
 
 ?>
