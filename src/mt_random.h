7be28c47152723db7f66efc71820c89d6ccdf11b
xrfind <xrfind@gmail.com>
2014-03-04 18:09:08 +0100

    minor

--- src/mt_random.h
+++ src/mt_random.h
@@ -1,13 +1,13 @@
-#ifndef CN_RANDOM_H
-#define CN_RANDOM_H
+#ifndef MT_RANDOM_H
+#define MT_RANDOM_H
 
 void init_by_array(unsigned long init_key[], int key_length);
-unsigned long genrand_int32(void);
-long genrand_int31(void);
-double genrand_real1(void);
-double genrand_real2(void);
-double genrand_real3(void);
-double genrand_res53(void);
+unsigned long 	genrand_int32(void);
+long 			genrand_int31(void);
+double 			genrand_real1(void);
+double 			genrand_real2(void);
+double 			genrand_real3(void);
+double 			genrand_res53(void);
 
 //int verify_random(void);
 
