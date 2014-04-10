5a502ce226809070af1cf63c9ff8741ce0a6c78e
xrfind <xrfind@gmail.com>
2014-04-10 23:02:41 +0200

    going on

--- src/linefile.h
+++ src/linefile.h
@@ -1,1 +1,22 @@
+#ifndef CN_LINEFILE_H
+#define CN_LINEFILE_H
 
+struct LineFile {
+	int *i1;
+	int i1Max;
+	int i1Min;
+
+	int *i2;
+	int i2Max;
+	int i2Min;
+
+	int *d1;
+	int d1Max;
+	int d2Min;
+
+	long linesNum;
+};
+
+struct LineFile *create_LineFile(const char * const filename, ...);
+
+#endif
---
+++ src/mtprand.h
@@ -0,0 +1,12 @@
+#ifndef CN_MTPRAND_H
+#define CN_MTPRAND_H
+
+void init_by_array(unsigned long init_key[], int key_length);
+unsigned long genrand_int32(void);
+long genrand_int31(void);
+double genrand_real1(void);
+double genrand_real2(void);
+double genrand_real3(void);
+double genrand_res53(void);
+
+#endif
